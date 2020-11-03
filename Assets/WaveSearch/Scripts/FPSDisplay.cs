b6475ce81e2421bd45a40e562e81ae4c3b649878
1111111 <111111111>
2020-11-03 13:07:10 +0800

    Upgrade to Unity 2020.1

--- Assets/WaveSearch/Scripts/FPSDisplay.cs
+++ Assets/WaveSearch/Scripts/FPSDisplay.cs
@@ -1,31 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-namespace RockVR.Common
+
+public class FPSDisplay : MonoBehaviour
 {
-    public class FPSDisplay : MonoBehaviour
+    /// <summary>
+    /// Delta time
+    /// </summary>
+    float deltaTime = 0.0f;
+
+    /// <summary>
+    /// It will be used for printing out fps text on screen
+    /// </summary>
+    Text text;
+
+    void Start()
     {
-        /// <summary>
-        /// Delta time
-        /// </summary>
-        float deltaTime = 0.0f;
+        text = GetComponent<Text>();
+    }
 
-        /// <summary>
-        /// It will be used for printing out fps text on screen
-        /// </summary>
-        Text text;
-
-        void Start()
-        {
-            text = GetComponent<Text>();
-        }
-
-        void Update()
-        {
-            deltaTime = (Time.deltaTime ) ;
-            float msec = deltaTime * 1000.0f;
-            float fps = 1.0f / deltaTime;
-            text.text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        }
+    void Update()
+    {
+        deltaTime = (Time.deltaTime ) ;
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        text.text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # URPOcean
 
-Require Unity 2019.4.0f1 or later
+Require Unity 2020.1.0f1 or later
 
 ![](./Image/URPOcean.png)
 ![](./Image/URPOcean1.png)
